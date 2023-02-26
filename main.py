d76fa0e1249212a8ae5f85036248601b20e25412
ishakoregave <65064098+ishakoregave@users.noreply.github.com>
2023-02-26 09:23:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_agentTips(key):
     for i in result:
         hashmap[hashmap1[i['Idx']]] = i['Tips']
     print(key)
-    return(hashmap['astra'])
+    return(hashmap)
     #print(hashmap)
 
 
